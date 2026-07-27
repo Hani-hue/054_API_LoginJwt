54bd6faa8cc4d8d2dcd4fb43d1b084b60ec7092f
Hani-hue <isnahabibah9827@gmail.com>
2026-07-27 14:49:48 +0700

    Menambahkan fungsi startServer untuk inisialisasi aplikasi

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', require('./routes/api'));
+async function startServer() {
+    await connectDatabase();
+    app.listen(PORT, () => {
+        console.log(`Server is running at http://localhost:${PORT}`);
+  });
+}
+
+startServer();
